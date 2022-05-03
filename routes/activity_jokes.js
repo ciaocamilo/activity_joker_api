dfa2e150b772ac745423b6457ae80a217a4a6784
Camilo A <ing_camiloacg@hotmail.com>
2022-05-03 03:56:10 -0500

    Agrega función de consultarr actividades y chistes

--- routes/activity_jokes.js
+++ routes/activity_jokes.js
@@ -1,8 +1,51 @@
 import express from 'express';
+import axios from 'axios';
 const router = express.Router();
 
 // Importar el modelo
 import ActivityJokes from '../models/activity_jokes';
+
+// Ruta para generar actividad y chiste
+router.get('/activity-jokes/new/:type', async(req, res) => {
+    const activity_type = req.params.type;
+    try {
+        axios.get('https://www.boredapi.com/api/activity?type=' + activity_type)
+            .then(result => {
+                if (result.data.error) {
+                    res.json({error: "El tipo de actividad no es válido. Pista: Debe ser en inglés."});
+                } else {
+                    const activity_data = result.data;
+                    const words_array = activity_data.activity.split(" ");
+                    const jokes_array = [];
+                    words_array.forEach((word, index) => {
+                        axios.get('https://v2.jokeapi.dev/joke/Any?contains=' + word)
+                            .then(result2 => {
+                                if (!result2.data.error) {
+                                    if (jokes_array.length < 1) {
+                                        jokes_array.push({joke: result2.data});
+                                        res.json(jokes_array[0]);
+                                    }
+                                }
+                                if (index === words_array.length-1 && jokes_array.length === 0) {
+                                    res.json({error: "Esta es una actividad muy aburrida"});
+                                }
+                            })
+                            .catch(error => {
+                                console.log(error);
+                            })
+                    });
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Se presentó un error al generar la actividad y/o chiste',
+            error
+        })
+    }
+});
 
 // Ruta para consultar todos las actividades guardadas y sus chistes
 router.get('/activity-jokes/download', async(req, res) => {
